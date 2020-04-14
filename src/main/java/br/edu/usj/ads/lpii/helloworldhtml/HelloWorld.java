94eb22d3d3d09e4fc123e74ca683e59c201fbc8d
Osmar da Cunha Filho <osmarcf@gmail.com>
2020-04-14 11:38:39 -0300

    primeiro commit

---
+++ src/main/java/br/edu/usj/ads/lpii/helloworldhtml/HelloWorld.java
@@ -0,0 +1,18 @@
+package br.edu.usj.ads.lpii.helloworldhtml;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HelloWorld {
+
+    @GetMapping(value="helloWorld")
+    public ModelAndView getHelloWorld() {
+        ModelAndView modelAndView = new ModelAndView("hello-world");
+
+        return modelAndView;
+    }
+    
+}
